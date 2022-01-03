7dc6f3cc9a3c0a658ed0eccb52c0474da86a1412
Dudu Joseph <dudu.joseph@outlook.com>
2022-01-03 23:40:29 +0530

    This update has rectified the mesage trucation because of the & sign and the issue with the , for the total quantity and the sold quantity variables.

---
+++ test.py
@@ -0,0 +1,5 @@
+import sys 
+print("The script has the name: %s" % (sys.argv[0]))
+arguments = len(sys.argv)
+print("The script has %i arguments" % (arguments))
+print("Argument 2 is: %s" % (sys.argv[1]))
